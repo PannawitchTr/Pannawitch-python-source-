a034d6d435539b6a01616f75e8c736de16db1f8c
Pannawitch Trathongkum <pannwitch.t@ku.th>
2026-09-25 11:52:23 +0700

    assignment class

--- assignments/week07/quiz-1.py
+++ assignments/week07/quiz-1.py
@@ -12,13 +12,30 @@ class Rectangle:
 
     # Method to get the area
     def get_area(self):
-        pass
+        self.area = self.length * self.width #alternative way (return self.length * self.width)
+        return f"คืนค่าพื้นที่ของสี่เหลี่ยม = {self.area}"
+        
 
     # Method to get the perimeter
     def get_perimeter(self):
-        pass
-
+        self.permimeter = 2 * (self.width + self.length) #alternative way (retrun f"Perimeter = {self.length} * {self.width} = {2 * (self.length * self.width)}"")
+        return f"คืนค่ารอบรูปของสี่เหลี่ยม = {self.permimeter}"
 
 rect = Rectangle(10, 5)
 print(rect.get_area())       # Should print 50
 print(rect.get_perimeter())  # Should print 30
+
+"""
+ขอให้เขียนคลาส Circle ที่ทำงานคล้ายกับคลาส Rectangle
+
+"""
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def get_radius(self):
+        return f"radius = 2 * 3.14 * {self.radius} = {2 * 3.14 * self.radius}"
+
+rect = Circle(10)
+print(rect.get_radius()) 
--- labs/week07-lab/example/class-draft.py
+++ labs/week07-lab/example/class-draft.py
@@ -1,15 +1,33 @@
+"""
+2 types of programing
+1. structured programing ==> การเขียนโปรแกรมเชิงโครงสร้าง (โปรแกรมที่เขียนแบบนี้ส่วนใหญ่)> c, javascript, php, python
+2. object-oriented programing(OOP) ==> การเขียนโปรแกรมเชิงวัตถุ > java, C#, python  
+"""
+
 class ClassName:
     """Class docstring"""
-    
+
+    #ข้อมูลที่ต้องใช้ในการแก้ปัญหา ระบุไว้ใน contructor metod
     def __init__(self, parameters):
         # Constructor method
         self.attribute = value
-    
+
+    #การกระทำ ==> method
     def method_name(self):
         # Instance method
         return something
 
+    def method_name(self):
+        pass
 
+# การสร้างวัตถุจากคลาส ==> เอาคลาสมาใช้
 myObj = ClassName(parameters)
+
+#ใช้งานวัตถุจากคลาส
 print(myObj.attribute)
 resultFromMethod = myObj.method_name()
+
+myObj2 = ClassName(parameters)
+print(myObj2.attibute)
+print(myObj2.method_name())
+myObj2.method_name2()
